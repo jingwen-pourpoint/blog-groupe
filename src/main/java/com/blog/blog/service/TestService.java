91b782949b25d64a17f3c31beac702ad3990630e
Jingwen Pourpoint <jingwen.pourpoint@gmail.com>
2022-01-27 09:51:10 +0100

    Initial commit

---
+++ src/main/java/com/blog/blog/service/TestService.java
@@ -0,0 +1,11 @@
+package com.blog.blog.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public String callService() {
+        return "Réponse du service";
+    }
+}
